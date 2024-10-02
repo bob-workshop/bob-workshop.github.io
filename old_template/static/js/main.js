b8958c434369ebbd2981b3cea42e41cb0763c4c5
Daniele Cattaneo <cattaneo@informatik.uni-freiburg.de>
2024-10-02 10:16:24 +0200

    update

--- old_template/static/js/main.js
+++
@@ -1,8 +0,0 @@
-// Bibtex
-function toggle_visibility(block_id) {
-	var e = document.getElementById(block_id);
-	if (e.style.display == 'block')
-		e.style.display = 'none';
-	else
-		e.style.display = 'block';
-}
